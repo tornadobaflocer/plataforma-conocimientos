4712c0a647229fff47db7f0c90f3709bb257121e
tornadobaflocer <jhudsubi@hotmail.com>
2025-08-14 23:11:38 -0500

    Add comment for server initialization

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const app = require('./src/app');
 const PORT = process.env.PORT || 5000;
 
+// Iniciar el servidor y escuchar en el puerto especificado
 app.listen(PORT, () => {
     console.log(`Servidor escuchando en el puerto ${PORT}`);
 });
